e82181f87aa301821851e46fd86d81ab5be8304c
JamesDeyyy <JamesDey07@outlook.com>
2025-12-06 14:56:43 +0000

    Coins and Timer Changes and Changes to Levels

--- CMP112 INDIVIDUAL/Assets/Scripts/nextLevel.cs
+++ CMP112 INDIVIDUAL/Assets/Scripts/nextLevel.cs
@@ -1,25 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class nextLevel : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int level = 0;
-    void Start()
-    {
-    }
+    public GameObject scoreText;
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Win"))
         {
+            Destroy(scoreText);
             AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(level);
         }
     }
+
 }
